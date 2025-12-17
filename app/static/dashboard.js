f8c2475d0584ba917e591d2625fbe9ca9e1dbec8
Golan <dukspam@gmail.com>
2025-12-17 02:07:59 +0200

    remove unused func

--- app/static/dashboard.js
+++ app/static/dashboard.js
@@ -1,23 +1,3 @@
-function handleDelete(event, form) {
-    event.preventDefault(); // Prevent default form submission
-
-    fetch(form.action, {
-        method: 'POST',
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest'
-        }
-    })
-    .then(response => response.json())
-    .then(result => {
-        if (result.error) {
-            showToast('error', result.error);
-        } else {
-            location.reload();
-        }
-    })
-    .catch(error => console.error('Error:', error));
-}
-
 function toggleAll(type) {
     const boxes = document.querySelectorAll("." + type + "-box");
     const master = document.getElementById("checkAll" + type.charAt(0).toUpperCase() + type.slice(1));
