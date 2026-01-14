646221cae2155bb35ac3093b6776e498c7d4348d
Golan <dukspam@gmail.com>
2026-01-15 00:26:54 +0200

    toggleAll in dashboard and data pages

--- app/static/dashboard.js
+++ app/static/dashboard.js
@@ -5,3 +5,8 @@ function toggleAll(type) {
     const master = UTILS.getElementByIdOrThrow("checkAll" + type.charAt(0).toUpperCase() + type.slice(1));
     boxes.forEach(b => b.checked = master.checked);
 }
+
+UTILS.getElementByIdOrThrow('checkAllFasta')
+     .addEventListener('change', () => toggleAll('fasta'));
+UTILS.getElementByIdOrThrow('checkAllScore')
+     .addEventListener('change', () => toggleAll('score'));
